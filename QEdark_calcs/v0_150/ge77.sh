0cca23c7d1f38b0c925516bb722d34a5ecf034c9
MTalz <57528135+MTalz@users.noreply.github.com>
2020-10-16 16:24:31 +1100

    Uploaded via CAMK

---
+++ QEdark_calcs/v0_150/ge77.sh
@@ -0,0 +1,24 @@
+#! /bin/bash -l
+## job name
+#SBATCH -J v0_150
+## number of nodes
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH -c 10
+#SBATCH --mem-per-cpu=512MB
+#SBATCH --time=48:00:00
+## partition (queue) to use
+#SBATCH -p para
+#SBATCH –output="stdout.txt"
+#SBATCH –error="stderr.txt"
+
+## commands/steps to execute
+# go to the submission directory
+DIR=v0_150
+cd /home/mtalia/q-e-qe-5.1.2/QEdark_run/$DIR 
+
+# Set environment variables
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export OMP_STACKSIZE=512M
+
+/home/mtalia/q-e-qe-5.1.2/bin/pw.x < ge.77.in > ge.77.out 
